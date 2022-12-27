32badbddf727bb9d54446c51236d872f63cfb29b
김도완 <kdw0247@gmail.com>
2022-12-27 15:15:07 +0900

    theme css 적용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import Router from './routes/Routes';
 import { GlobalStyle } from './styles/GlobalStyle';
+import theme from './styles/theme';
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <Router />
-    </>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
